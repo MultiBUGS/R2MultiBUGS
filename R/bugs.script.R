MultiBUGS/R2MultiBUGS

commit 1f85d9c8be517f55c831fe5f99ffc3507171d356

Improve/standardise formatting of code

R/bugs.script.R
@@ -1,9 +1,8 @@
 #' Writes script for running MultiBUGS
-#' 
+#'
 #' Write file \file{script.txt} for \pkg{MultiBUGS} to read - intended for
 #' internal use
-#' 
-#' 
+#'
 #' @param parameters.to.save parameters that should be monitored
 #' @param n.chains number of Markov chains
 #' @param n.iter number of total iterations (including burn in)
@@ -53,110 +52,195 @@
 #' written
 #' @seealso The main function to be called by the user is \code{\link{bugs}}.
 #' @keywords internal file IO
-"bugs.script" <-
-  function(parameters.to.save, n.chains, n.iter, n.burnin,
-           n.thin, n.workers, saveExec, restart, model.file.bug,
-           model.file, debug=FALSE, is.inits, fix.founders,
-           DIC=FALSE, useWINE=FALSE,
-           newWINE=TRUE, WINEPATH=NULL, bugs.seed=NULL, summary.only=FALSE,
-           save.history=(.Platform$OS.type == "windows" | useWINE==TRUE),
-           bugs.data.file, bugs.inits.files,
-           over.relax = FALSE)
-{
+bugs.script <- function(parameters.to.save,
+                        n.chains,
+                        n.iter,
+                        n.burnin,
+                        n.thin,
+                        n.workers,
+                        saveExec,
+                        restart,
+                        model.file.bug,
+                        model.file,
+                        debug = FALSE,
+                        is.inits,
+                        fix.founders,
+                        DIC = FALSE,
+                        useWINE = FALSE,
+                        newWINE = TRUE,
+                        WINEPATH = NULL,
+                        bugs.seed = NULL,
+                        summary.only = FALSE,
+                        save.history = (.Platform$OS.type == "windows" |
+                                          useWINE == TRUE),
+                        bugs.data.file,
+                        bugs.inits.files,
+                        over.relax = FALSE){
   # restart not suppored in MultiBUGS at the moment afaik
   if (restart){
     stop("restart = TRUE is not yet supported by R2MultiBUGS")
   }
 
   ## Write file script.txt for Bugs
-  if(n.iter - n.burnin < 2)
-    stop ("(n.iter-n.burnin) must be at least 2")
+  if (n.iter - n.burnin < 2){
+    stop("(n.iter-n.burnin) must be at least 2")
+  }
   working.directory <- getwd()
   script <- "script.txt"
 
-  model <- 
-    if (length(grep("\\\\", model.file)) || length(grep("/", model.file))) {
-        gsub("\\\\", "/", model.file)
-    }
-    else file.path(working.directory, model.file)
-  model <- native2win(model, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  model <- if (length(grep("\\\\", model.file)) ||
+                 length(grep("/", model.file))){
+    gsub("\\\\", "/", model.file)
+  } else {
+    file.path(working.directory, model.file)
+  }
+  model <- native2win(model,
+                      useWINE = useWINE,
+                      newWINE = newWINE,
+                      WINEPATH = WINEPATH)
 
   data <- file.path(working.directory, bugs.data.file)
-  data <- native2win(data, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  data <- native2win(data,
+                     useWINE = useWINE,
+                     newWINE = newWINE,
+                     WINEPATH = WINEPATH)
 
-  coda  <- file.path(working.directory, "/")
-  coda <- native2win(coda, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  coda <- file.path(working.directory, "/")
+  coda <- native2win(coda,
+                     useWINE = useWINE,
+                     newWINE = newWINE,
+                     WINEPATH = WINEPATH)
 
-  model.file.bug<-file.path(working.directory,model.file.bug)
-  model.file.bug<-native2win(model.file.bug, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  model.file.bug <- file.path(working.directory, model.file.bug)
+  model.file.bug <- native2win(model.file.bug,
+                               useWINE = useWINE,
+                               newWINE = newWINE,
+                               WINEPATH = WINEPATH)
 
   logFile <- file.path(working.directory, "log.odc")
-  logFile <- native2win(logFile, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  logFile <- native2win(logFile,
+                        useWINE = useWINE,
+                        newWINE = newWINE,
+                        WINEPATH = WINEPATH)
   logFileTxt <- file.path(working.directory, "log.txt")
-  logFileTxt <- native2win(logFileTxt, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)
+  logFileTxt <- native2win(logFileTxt,
+                           useWINE = useWINE,
+                           newWINE = newWINE,
+                           WINEPATH = WINEPATH)
 
-  inits <- paste(working.directory, "/", bugs.inits.files, sep="")
-  inits <- sapply(inits, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH, 
-    function(x, useWINE, newWINE, WINEPATH) 
-    {native2win(x, useWINE=useWINE, newWINE=newWINE, WINEPATH=WINEPATH)})
+  inits <- paste(working.directory, "/", bugs.inits.files, sep = "")
+  inits <- sapply(inits,
+                  useWINE = useWINE,
+                  newWINE = newWINE,
+                  WINEPATH = WINEPATH,
+                  function(x, useWINE, newWINE, WINEPATH){
+                    native2win(x,
+                               useWINE = useWINE,
+                               newWINE = newWINE,
+                               WINEPATH = WINEPATH)
+                  })
 
-  initlist <- paste("modelInits(", "'", inits, "',",1:n.chains,")\n", sep="")
+  initlist <- paste("modelInits(", "'", inits, "',", 1:n.chains, ")\n",
+                    sep = "")
 
   if (fix.founders){
-    geninitlist  <- "modelGenInits()\n"
+    geninitlist <- "modelGenInits()\n"
   } else {
     geninitlist <- "modelGenInits(\"F\")\n"
   }
-  
-  savelist <- paste("samplesSet(", parameters.to.save, ")\n", sep="")
-  summarylist <- paste("summarySet(", parameters.to.save, ")\n", sep="")
+
+  savelist <- paste("samplesSet(", parameters.to.save, ")\n", sep = "")
+  summarylist <- paste("summarySet(", parameters.to.save, ")\n", sep = "")
 
   bugs.seed.cmd <- ""
   if (!is.null(bugs.seed)){
-    bugs.seed.cmd <- paste("modelSetRN(", bugs.seed, ")\n", sep="")
+    bugs.seed.cmd <- paste("modelSetRN(", bugs.seed, ")\n", sep = "")
   }
-    
-  thinUpdate <- paste("modelUpdate(", formatC(n.burnin, format='d'), ",", n.thin, 
-                      ",",formatC(n.burnin, format='d'), ")\n", sep="")
+
+  thinUpdate <- paste("modelUpdate(",
+                      formatC(n.burnin, format = "d"),
+                      ",",
+                      n.thin,
+                      ",",
+                      formatC(n.burnin, format = "d"), ")\n",
+                      sep = "")
 
   cat(
-    if(.Platform$OS.type == "windows" | useWINE) "modelDisplay('log')\n",
-    if(restart)c("modelInternalize('", model.file.bug, "')\n"),
-    if(restart && n.burnin>0)c(
-       "samplesClear('*')\n",
-       "summaryClear('*')\n"
-       ),
-    if(!restart)
-      c( 
-      "modelCheck('", model, "')\n",
-      "modelData('", data, "')\n",
-      "modelCompile(", n.chains, ")\n"
-    ),
-    if(!restart)bugs.seed.cmd,
-    if(!restart && is.inits) initlist,
-    if(!restart) geninitlist,
-    if(!restart)"modelDistribute(", n.workers, ")\n",
-    if(!restart && over.relax) 'over.relax("yes")\n',
-    if((!restart) || (n.burnin>0))c(
-    thinUpdate,
-    savelist,
-    summarylist
-    ),
-    if(((!restart) || (n.burnin>0)) && DIC) "dicSetS()\n",
-    "modelUpdate(", formatC(n.iter-n.burnin, format='d'), ",", n.thin, 
-                  ",",formatC(n.iter-n.burnin, format='d'),")\n", 
-    "samplesCoda('*', '", coda, "')\n", 
+    if (.Platform$OS.type == "windows" | useWINE){
+      "modelDisplay('log')\n"
+    },
+    if (restart){
+      c("modelInternalize('", model.file.bug, "')\n")
+    },
+    if (restart && n.burnin > 0){
+      c("samplesClear('*')\n", "summaryClear('*')\n")
+    },
+    if (!restart){
+      c("modelCheck('", model, "')\n",
+        "modelData('", data, "')\n",
+        "modelCompile(", n.chains, ")\n")
+    },
+    if (!restart){
+      bugs.seed.cmd
+    },
+    if (!restart && is.inits){
+      initlist
+    },
+    if (!restart){
+      geninitlist
+    },
+    if (!restart){
+      "modelDistribute(", n.workers, ")\n"
+    },
+    if (!restart && over.relax){
+      "over.relax(\"yes\")\n"
+    },
+    if ((!restart) || (n.burnin > 0)){
+      c(thinUpdate, savelist, summarylist)
+    },
+    if (((!restart) || (n.burnin > 0)) && DIC){
+      "dicSetS()\n",
+      "modelUpdate(",
+      formatC(n.iter - n.burnin, format = "d"),
+      ",",
+      n.thin,
+      ",",
+      formatC(n.iter - n.burnin, format = "d"),
+      ")\n",
+      "samplesCoda('*', '",
+      coda,
+      "')\n"
+    },
     "summaryStats('*')\n",
-    if(DIC) "dicStats()\n",
-    if (save.history) "samplesHistory('*')\n", 
-    if(saveExec)c("modelExternalize('",model.file.bug,"')\n"),
-    if(.Platform$OS.type == "windows" | useWINE) c("modelSaveLog('", logFile, "')\n",
-    "modelSaveLog('", logFileTxt, "')\n"),
-    file=script, sep="", append=FALSE)
+    if (DIC){
+      "dicStats()\n"
+    },
+    if (save.history){
+      "samplesHistory('*')\n"
+    },
+    if (saveExec){
+      c("modelExternalize('", model.file.bug, "')\n")
+    },
+    if (.Platform$OS.type == "windows" | useWINE){
+      c("modelSaveLog('",
+        logFile,
+        "')\n", "modelSaveLog('",
+        logFileTxt,
+        "')\n")
+    },
+    file = script,
+    sep = "",
+    append = FALSE
+  )
 
-    if(!debug) cat("modelQuit('y')\n", file=script, append=TRUE)
+  if (!debug){
+    cat("modelQuit('y')\n", file = script, append = TRUE)
+  }
 
-  sims.files <- paste("CODAchain", 1:n.chains, ".txt", sep="")
-  for(i in 1:n.chains)
-    cat("MultiBUGS did not run correctly.\n", file=sims.files[i], append=FALSE)
+  sims.files <- paste("CODAchain", 1:n.chains, ".txt", sep = "")
+  for (i in 1:n.chains){
+    cat("MultiBUGS did not run correctly.\n",
+        file = sims.files[i],
+        append = FALSE)
+  }
 }

R/R2MultiBUGS-package.R
@@ -1,9 +1,9 @@
 #' Running MultiBUGS from R
-#' 
+#'
 #' \pkg{R2MultiBUGS} Call a \pkg{BUGS} model, summarize inferences and
 #' convergence in a table and graph, and save the simulations in arrays for
 #' easy access in \code{R}. The main command is \code{\link{bugs}}.
-#' 
+#'
 #' The following are sources of information on \pkg{R2MultiBUGS} package:
 #' \tabular{ll}{ DESCRIPTION file\tab \code{library(help="R2MultiBUGS")}\cr
 #' \tab \cr This file\tab \code{package?R2MultiBUGS}\cr \tab \cr Vignette\tab
@@ -11,7 +11,7 @@
 #' \code{\link{bugs}}\cr \tab \code{\link{write.model}}\cr \tab
 #' \code{\link{print.bugs}}\cr \tab \code{\link{plot.bugs}}\cr \tab \cr
 #' News\tab \code{file.show(system.file("NEWS", package="R2MultiBUGS"))}\cr }
-#' 
+#'
 #' @name R2MultiBUGS-package
 #' @aliases R2MultiBUGS-package R2MultiBUGS
 #' @docType package
@@ -25,10 +25,9 @@
 NULL
 
 #' 8 schools analysis
-#' 
+#'
 #' 8 schools analysis
-#' 
-#' 
+#'
 #' @name schools
 #' @docType data
 #' @format A data frame with 8 observations on the following 3 variables.
@@ -36,7 +35,7 @@ NULL
 #' \item{sd}{See Source.} }
 #' @source Rubin, D.B. (1981): Estimation in Parallel Randomized Experiments.
 #' \emph{Journal of Educational Statistics} 6(4), 377-400.
-#' 
+#'
 #' Section 5.5 of Gelman, A., Carlin, J.B., Stern, H.S., Rubin, D.B. (2003):
 #' \emph{Bayesian Data Analysis}, 2nd edition, CRC Press.
 #' @keywords datasets

R/as.bugs.array.R
@@ -1,10 +1,10 @@
 #' Convert to bugs object
-#' 
+#'
 #' Function converting results from Markov chain simulations, that might not be
 #' from BUGS, to bugs object. Used mainly to display results with
 #' \code{\link{plot.bugs}}.
-#' 
-#' 
+#'
+#'
 #' This function takes a 3-way array of simulations and makes it into a
 #' \code{\link{bugs}} object that can be conveniently displayed using
 #' \code{print} and \code{plot} and accessed using \code{attach.bugs}.  If the
@@ -15,10 +15,10 @@
 #' length 8, and mu and tau are scalar parameters. These will all be plotted
 #' appropriately by \code{plot} and attached appropriately by
 #' \code{attach.bugs}.
-#' 
+#'
 #' If \code{DIC=TRUE} then DIC can be either already passed to argument
 #' \code{DICOutput} or calculated from deviance values in \code{sims.array}.
-#' 
+#'
 #' @param sims.array 3-way array of simulation output, with dimensions n.keep,
 #' n.chains, and length of combined parameter vector.
 #' @param model.file file containing the model written in \pkg{MultiBUGS} code
@@ -39,33 +39,36 @@
 #' @seealso \code{\link{bugs}}
 #' @keywords interface manip
 #' @export as.bugs.array
-as.bugs.array <- function(sims.array, model.file=NULL, program=NULL,
-                          DIC=FALSE, DICOutput=NULL,
-                          n.iter=NULL, n.burnin=0, n.thin=1)
-{
-  ## Jouni Kerman's function to convert a 3-way array to a Bugs object
-  ##
-  ## 'sims.array' is supposed to be a 3-way array with
-  ## n.sims*n.chains*n.parameters simulations, and
-  ## the 3rd component of dimnames(x) should have the parameter names.
+as.bugs.array <- function(sims.array,
+                          model.file = NULL,
+                          program = NULL,
+                          DIC = FALSE,
+                          DICOutput = NULL,
+                          n.iter = NULL,
+                          n.burnin = 0,
+                          n.thin = 1){
+  ## Jouni Kerman's function to convert a 3-way array to a Bugs
+  ## object 'sims.array' is supposed to be a 3-way array with
+  ## n.sims*n.chains*n.parameters simulations, and the 3rd
+  ## component of dimnames(x) should have the parameter names.
   d <- dim(sims.array)
-  n.keep       <- d[1]
-  n.chains     <- d[2]
+  n.keep <- d[1]
+  n.chains <- d[2]
   n.parameters <- d[3]
-  n.sims       <- n.keep*n.chains
-  if (is.null(n.iter)) n.iter <- (n.keep + n.burnin)*n.thin
-  #
+  n.sims <- n.keep * n.chains
+  if (is.null(n.iter)){
+    n.iter <- (n.keep + n.burnin) * n.thin
+  }
   parameter.names <- dimnames(sims.array)[[3]]
-  if (is.null(parameter.names)) {
-    parameter.names <- paste("P", 1:n.parameters, sep="")
+  if (is.null(parameter.names)){
+    parameter.names <- paste("P", 1:n.parameters, sep = "")
     dimnames(sims.array)[[3]] <- parameter.names
   }
-  parameters.to.save <- unique(sapply(strsplit(parameter.names,"\\["),"[",1))
-  #
+  parameters.to.save <- unique(sapply(strsplit(parameter.names, "\\["), "[", 1))
   sims <- matrix(NA, n.sims, n.parameters)
   root.long <- character(n.parameters)
   indexes.long <- vector(n.parameters, mode = "list")
-  for (i in 1:n.parameters) {
+  for (i in 1:n.parameters){
     temp <- decode.parameter.name(parameter.names[i])
     root.long[i] <- temp$root
     indexes.long[[i]] <- temp$indexes
@@ -73,31 +76,33 @@ as.bugs.array <- function(sims.array, model.file=NULL, program=NULL,
   n.roots <- length(parameters.to.save)
   left.bracket.short <- as.vector(regexpr("[[]", parameters.to.save))
   right.bracket.short <- as.vector(regexpr("[]]", parameters.to.save))
-  root.short <- ifelse(left.bracket.short == -1, parameters.to.save,
-    substring(parameters.to.save, 1, left.bracket.short - 1))
+  root.short <- ifelse(left.bracket.short == -1,
+                       parameters.to.save,
+                       substring(parameters.to.save, 1, left.bracket.short - 1))
   dimension.short <- rep(0, n.roots)
   indexes.short <- vector(n.roots, mode = "list")
   n.indexes.short <- vector(n.roots, mode = "list")
   long.short <- vector(n.roots, mode = "list")
   length.short <- numeric(n.roots)
-  for (j in 1:n.roots) {
+  for (j in 1:n.roots){
     long.short[[j]] <- (1:n.parameters)[root.long == root.short[j]]
     length.short[j] <- length(long.short[[j]])
     if (length.short[j] == 0){
       stop(paste("parameter", root.short[[j]], "is not in the model"))
-    }
-    else if (length.short[j] > 1) {
+    } else if (length.short[j] > 1){
       dimension.short[j] <- length(indexes.long[[long.short[[j]][1]]])
       n.indexes.short[[j]] <- numeric(dimension.short[j])
       for (k in 1:dimension.short[j]){
-        n.indexes.short[[j]][k] <- length(unique(unlist(lapply(indexes.long[long.short[[j]]], .subset, k))))
+        n.indexes.short[[j]][k] <-
+          length(unique(unlist(lapply(indexes.long[long.short[[j]]],
+                                      .subset,
+                                      k))))
       }
       length.short[j] <- prod(n.indexes.short[[j]])
-### bug reported by S. McKay Curtis on February 22, 2010, we cannot check that safely:
-##      if (length(long.short[[j]]) != length.short[j]){
-##        stop(paste("error in parameter", root.short[[j]],
-##          "in parameters.to.save"))
-##      }
+      ### bug reported by S. McKay Curtis on February 22, 2010, we
+      ### cannot check that safely: if (length(long.short[[j]]) !=
+      ### length.short[j]){ stop(paste('error in parameter',
+      ### root.short[[j]], 'in parameters.to.save')) }
       indexes.short[[j]] <- as.list(numeric(length.short[j]))
       for (k in 1:length.short[j]){
         indexes.short[[j]][[k]] <- indexes.long[[long.short[[j]][k]]]
@@ -105,81 +110,107 @@ as.bugs.array <- function(sims.array, model.file=NULL, program=NULL,
     }
   }
   rank.long <- unlist(long.short)
-  # -----
-  # yes, it's inefficient to do this, but for now I'm just letting this be as it is:
-  for (k in 1:n.parameters) {
-    sims[,k] <- as.vector(sims.array[,,k])
+  # ----- yes, it's inefficient to do this, but for now I'm
+  # just letting this be as it is:
+  for (k in 1:n.parameters){
+    sims[, k] <- as.vector(sims.array[, , k])
   }
   # ----
   dimnames(sims) <- list(NULL, parameter.names)
   summary <- monitor(sims.array, n.chains, keep.all = TRUE)
   last.values <- as.list(numeric(n.chains))
-  for (i in 1:n.chains) {
-    n.roots.0 <- if(DIC) n.roots - 1 else n.roots
+  for (i in 1:n.chains){
+    n.roots.0 <- if (DIC){
+      n.roots - 1
+    } else {
+      n.roots
+    }
     last.values[[i]] <- as.list(numeric(n.roots.0))
     names(last.values[[i]]) <- root.short[1:n.roots.0]
-    for (j in 1:n.roots.0) {
-      if (dimension.short[j] <= 1) {
+    for (j in 1:n.roots.0){
+      if (dimension.short[j] <= 1){
         last.values[[i]][[j]] <- sims.array[n.keep, i, long.short[[j]]]
         names(last.values[[i]][[j]]) <- NULL
-      }
-      else{
-        last.values[[i]][[j]] <- aperm(array(sims.array[n.keep, i,
-          long.short[[j]]], rev(n.indexes.short[[j]])), dimension.short[j]:1)
+      } else {
+        last.values[[i]][[j]] <-
+          aperm(array(sims.array[n.keep, i, long.short[[j]]],
+                      rev(n.indexes.short[[j]])),
+                dimension.short[j]:1)
       }
     }
   }
   sims <- sims[sample(n.sims), , drop = FALSE]
-  sims.list <- summary.mean <- summary.sd <- summary.median <- vector(n.roots,
-    mode = "list")
+  sims.list <-
+    summary.mean <- summary.sd <- summary.median <- vector(n.roots, mode = "list")
   names(sims.list) <- names(summary.mean) <- names(summary.sd) <- names(summary.median) <- root.short
-  for (j in 1:n.roots) {
-    if (length.short[j] == 1) {
+  for (j in 1:n.roots){
+    if (length.short[j] == 1){
       sims.list[[j]] <- sims[, long.short[[j]]]
       summary.mean[[j]] <- summary[long.short[[j]], "mean"]
       summary.sd[[j]] <- summary[long.short[[j]], "sd"]
       summary.median[[j]] <- summary[long.short[[j]], "50%"]
-    }
-    else {
+    } else {
       temp2 <- dimension.short[j]:1
       sims.list[[j]] <- aperm(array(sims[, long.short[[j]]],
-        c(n.sims, rev(n.indexes.short[[j]]))), c(1, (dimension.short[j]+1):2))
-      summary.mean[[j]] <- aperm(array(summary[long.short[[j]],
-        "mean"], rev(n.indexes.short[[j]])), temp2)
-      summary.sd[[j]] <- aperm(array(summary[long.short[[j]],
-        "sd"], rev(n.indexes.short[[j]])), temp2)
-      summary.median[[j]] <- aperm(array(summary[long.short[[j]],
-        "50%"], rev(n.indexes.short[[j]])), temp2)
+                                    c(n.sims, rev(n.indexes.short[[j]]))),
+                              c(1, (dimension.short[j] + 1):2))
+      summary.mean[[j]] <- aperm(array(summary[long.short[[j]], "mean"],
+                                       rev(n.indexes.short[[j]])),
+                                 temp2)
+      summary.sd[[j]] <- aperm(array(summary[long.short[[j]], "sd"],
+                                     rev(n.indexes.short[[j]])),
+                               temp2)
+      summary.median[[j]] <- aperm(array(summary[long.short[[j]], "50%"],
+                                         rev(n.indexes.short[[j]])),
+                                   temp2)
     }
   }
   summary <- summary[rank.long, ]
 
-  all <- list(n.chains = n.chains, n.iter = n.iter, n.burnin = n.burnin,
-    n.thin = n.thin, n.keep = n.keep, n.sims = n.sims,
-    sims.array = sims.array[,,rank.long,drop = FALSE], sims.list = sims.list,
-    sims.matrix = sims[, rank.long], summary = summary, mean = summary.mean,
-    sd = summary.sd, median = summary.median, root.short = root.short,
-    long.short = long.short, dimension.short = dimension.short,
-    indexes.short = indexes.short, last.values = last.values, program=program,
-    model.file=model.file)
+  all <- list(n.chains = n.chains,
+              n.iter = n.iter,
+              n.burnin = n.burnin,
+              n.thin = n.thin,
+              n.keep = n.keep,
+              n.sims = n.sims,
+              sims.array = sims.array[, , rank.long, drop = FALSE],
+              sims.list = sims.list,
+              sims.matrix = sims[, rank.long],
+              summary = summary,
+              mean = summary.mean,
+              sd = summary.sd,
+              median = summary.median,
+              root.short = root.short,
+              long.short = long.short,
+              dimension.short = dimension.short,
+              indexes.short = indexes.short,
+              last.values = last.values,
+              program = program,
+              model.file = model.file)
 
-  if(DIC && is.null(DICOutput)) { ## calculate DIC from deviance
+  if (DIC && is.null(DICOutput)){
+    ## calculate DIC from deviance
     deviance <- all$sims.array[, , dim(sims.array)[3], drop = FALSE]
     dim(deviance) <- dim(deviance)[1:2]
     pD <- numeric(n.chains)
     DIC <- numeric(n.chains)
-    for (i in 1:n.chains) {
+    for (i in 1:n.chains){
       pD[i] <- var(deviance[, i])/2
       DIC[i] <- mean(deviance[, i]) + pD[i]
     }
-    all <- c(all, list(isDIC=TRUE, DICbyR=TRUE,
-                       pD=mean(pD), DIC=mean(DIC)))
-  } else if(DIC && !is.null(DICOutput)) { ## use DIC from BUGS
-    all <- c(all, list(isDIC=TRUE, DICbyR=FALSE,
-                       pD=DICOutput[nrow(DICOutput),4],
-                       DIC=DICOutput[nrow(DICOutput),3]))
+    all <- c(all, list(isDIC = TRUE,
+                       DICbyR = TRUE,
+                       pD = mean(pD),
+                       DIC = mean(DIC)))
+  } else if (DIC && !is.null(DICOutput)){
+    ## use DIC from BUGS
+    all <- c(all,
+             list(isDIC = TRUE,
+                  DICbyR = FALSE,
+                  pD = DICOutput[nrow(DICOutput), 4],
+                  DIC = DICOutput[nrow(DICOutput), 3]))
   } else {
-    all <- c(all, isDIC=FALSE)
+    all <- c(all, isDIC = FALSE)
   }
   class(all) <- "bugs"
   all

R/as.mcmc.list.bugs.R
@@ -1,18 +1,19 @@
 #' @method as.mcmc.list bugs
 #' @export
-as.mcmc.list.bugs <- function(x, ...)
-{
-  if(!inherits(x, "bugs")) 
+as.mcmc.list.bugs <- function(x, ...){
+  if (!inherits(x, "bugs")){
     stop("Method as.mcmc.list.bugs() is only intended for bugs objects.")
-  if(dim(x$sims.array)[2] != x$n.chains)
+  }
+  if (dim(x$sims.array)[2] != x$n.chains){
     stop("Inconsistancy in bug object regarding the number of chains.")
+  }
   mclis <- vector("list", x$n.chains)
   strt <- x$n.burnin + 1
   end <- x$n.iter
   ord <- order(dimnames(x$sims.array)[[3]])
-  for(i in 1:x$n.chains) {
-    tmp1 <- x$sims.array[,i,ord]
-    mclis[[i]] <- mcmc(tmp1, start=strt, end=end, thin=x$n.thin)
+  for (i in 1:x$n.chains){
+    tmp1 <- x$sims.array[, i, ord]
+    mclis[[i]] <- mcmc(tmp1, start = strt, end = end, thin = x$n.thin)
   }
   as.mcmc.list(mclis)
-} 
+}

commit cf928c40b44785f2f56580d2961580086017144b

Actually run modelUpdate and samplesCode
Correct error introduced by 1f85d9c8be517f55c831fe5f99ffc3507171d356 in generated script

R/bugs.script.R
@@ -200,18 +200,18 @@ bugs.script <- function(parameters.to.save,
       c(thinUpdate, savelist, summarylist)
     },
     if (((!restart) || (n.burnin > 0)) && DIC){
-      c("dicSetS()\n",
-        "modelUpdate(",
-        formatC(n.iter - n.burnin, format = "d"),
-        ",",
-        n.thin,
-        ",",
-        formatC(n.iter - n.burnin, format = "d"),
-        ")\n",
-        "samplesCoda('*', '",
-        coda,
-        "')\n")
+      "dicSetS()\n"
     },
+    "modelUpdate(",
+    formatC(n.iter - n.burnin, format = "d"),
+    ",",
+    n.thin,
+    ",",
+    formatC(n.iter - n.burnin, format = "d"),
+    ")\n",
+    "samplesCoda('*', '",
+    coda,
+    "')\n",
     "summaryStats('*')\n",
     if (DIC){
       "dicStats()\n"