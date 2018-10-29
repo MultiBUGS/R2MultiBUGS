7c19104dbb85fa3f0b10c0cda1bee20e9c469c2f
rjbgoudie <rjbgoudie@users.noreply.github.com>
2018-10-29 13:29:00 +0000

    Print log to console on error by default

--- R/bugs.run.R
+++ R/bugs.run.R
@@ -71,9 +71,19 @@ bugs.run <- function(n.burnin,
   tmp <- scan("CODAchain1.txt", character(), quiet = TRUE, sep = "\n")
   tmp <- tmp[1:min(100, length(tmp))]
   if (length(grep("MultiBUGS did not run correctly", tmp)) > 0){
-    stop(paste("Look at the log file in ",
-               getwd(),
-               " and\ntry again with 'debug=TRUE' to figure out",
-               "what went wrong within MultiBUGS."))
+    logfile <- file.path(getwd(), "log.txt")
+    if (file.exists(logfile)){
+      logtext <- paste(readLines(con = logfile), collapse = "\n")
+      stop(paste0("MultiBUGS did not run correctly\n",
+                  "Log file:\n",
+                  "=================\n",
+                  logtext,
+                  "\n================="))
+    } else {
+      stop(paste("MultiBUGS did not run correctly.",
+                 "No log file was produced.",
+                 "It might help to try again with 'debug=TRUE' to figure out",
+                 "what went wrong within MultiBUGS."))
+    }
   }
 }
